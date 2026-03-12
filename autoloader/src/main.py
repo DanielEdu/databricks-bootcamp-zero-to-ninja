3447a694096c3dea1f8f0734b4418535218e1edb
Daniel Portugal Revilla <danieleportugal@yape.com.pe>
2026-03-11 23:59:30 -0500

    autolaoder asset bundel

---
+++ autoloader/src/main.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Ingesta Landing a Bronze con Auto Loader
+# MAGIC
+# MAGIC Este proyecto implementa la ingesta de datos desde la capa **Landing** hacia **Bronze** usando **Databricks Auto Loader**.
+# MAGIC
+# MAGIC La solución está estructurada como un proyecto tipo **Databricks Asset Bundle**.
+
+# COMMAND ----------
+
+print("hola mundo")
